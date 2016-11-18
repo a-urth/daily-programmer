1984f76614c5f426fef450ef097bbf8ecbdc1bf6
Andrii Ursulenko <muaythaimanggon@gmail.com>
2016-11-18 09:30:24 +0200

    heavily optimized unsorten func for easy after reviewing for post

--- 20161115_easy/range_parsing.clj
+++ 20161115_easy/range_parsing.clj
@@ -1,23 +1,22 @@
 (use '[clojure.string :only [split split-lines join]])
 
+(defn pow10 [i] (reduce * (repeat i 10)))
+
 (defn unshorten [x y]
-  (let [iy (Integer. y)
-        ix (Integer. x)
-        l-delta (- (count (str x)) (count (str y)))]
-    (if (< iy ix)
-      (loop [ty (+ iy (reduce * (repeat l-delta 10)))
-             tx ix]
-        (if (and (> ty tx) (.endsWith (str ty) y))
-          ty
-          (recur (inc ty) tx)))
-      iy)))
+  (if (> y x)
+    y
+    (let [l-y (count (str y))
+          pred-x (quot x (pow10 l-y))
+          last-x (mod x (pow10 l-y))
+          pred (if (< last-x y) pred-x (inc pred-x))]
+      (Integer. (str pred y)))))
 
 (defn range-part [acc s]
-  (let [[t-start t-end t-step] (split s #"-|:|\.{2}")
+  (let [[t-start t-end t-step] (map #(Integer. %) (split s #"-|:|\.{2}"))
         n (last acc)
-        start (if (nil? n) (Integer. t-start) (unshorten n t-start))
+        start (if (nil? n) t-start (unshorten n t-start))
         end (inc (if (nil? t-end) start (unshorten start t-end)))
-        step (if (nil? t-step) 1 (Integer. t-step))]
+        step (if (nil? t-step) 1 t-step)]
     (into acc (range start end step))))
 
 (defn parse-range [s]
